718e2cbcf514007ea697a976d0c1a7acc092d13e
Nisar <82707815+MDNISARAHMED@users.noreply.github.com>
2021-06-06 02:32:37 +0530

    Computer
    3. JavaScript output

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// document.getElementById("heading").innerHTML="JavaScript is light weight object oriented programming langauge";
+//1. Writing into HTML Element.
+document.getElementById("heading").innerHTML="JavaScript is general purpose scripting language";
+
+//2. Writing into HTML output.
+document.write("JavaScript conforms to ECMA Script specification");
+
+//3. Writing into Alert box.
+window.alert("JavaScript is a web programming language!");
+
+//4. Writing into Browswer's console.
+console.log("JavaScript is a lightwieght object oriented programming language.");
