25e013bfdea8935bc8516bb286eed5e6e864cfde
Nisar <82707815+MDNISARAHMED@users.noreply.github.com>
2021-06-06 02:13:07 +0530

    Computer
    2. How to integrate JavaScript into HTML?

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-// WHAT YOU CAN DO WITH JSCRIPT?
-
-//1. JS can change the HTML Content
-// document.getElementById("heading").innerHTML="JScript"; 
-
-//2. JS can change the HTML Attribute values.
-// document.getElementById("img").src="https://www.hellomagazine.com/imagenes/film/20210524113858/marvels-eternals-angelina-jolie-first-look-trailer/0-550-309/angelina-jolie-1-t.jpg"
-
-//3. JS can change HTML Styles.
-// document.getElementById("img").style.border="2px solid red";
-
-//4. JS can hide amd show HTML Elements.
-// document.getElementById("img").style.display="none";
-// document.getElementById("img").style.display="block";
+// document.getElementById("heading").innerHTML="JavaScript is light weight object oriented programming langauge";
